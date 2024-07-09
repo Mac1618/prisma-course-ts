9d5ebd223e62cbf0ab65ef982fe422e36259de7f
Mac1618 <mac46099@gmail.com>
2024-07-09 16:05:48 +0800

    feat: Set up Prisma with SQLite and created Models
    - Install dev dependencies and initialize TypeScript
    
    - Install and initialize Prisma
    
    - Configure 'schema.prisma' with SQLite and define User model and Article model
    
    - Migrate database with Prisma
    
    - Add sample user data with Prisma

---
+++ index.ts
@@ -0,0 +1,31 @@
+// Prisma Library
+import { PrismaClient } from '@prisma/client';
+
+// PrismaClient variable
+const prisma = new PrismaClient();
+
+const main = async () => {
+	// CREATE User
+	const user = await prisma.user.create({
+		data: {
+			name: 'John Doe',
+			email: 'john@gmail.com',
+		},
+	});
+	console.log(user);
+};
+
+// call to run the function
+main()
+	.then(async () => {
+		// disconnect to database
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		// Log the erro
+		console.error(e);
+
+		// stop the process and disconnect
+		await prisma.$disconnect();
+		process.exit(1);
+	});
